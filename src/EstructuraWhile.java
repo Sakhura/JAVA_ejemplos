474ad82dbec73cfac6b7ce197977c714731db53a
sakhura <sabina.romero@gmail.com>
2026-02-09 20:13:32 -0300

    while ejercicio 10 nros

--- src/EstructuraWhile.java
+++ src/EstructuraWhile.java
@@ -1,2 +1,14 @@
 public class EstructuraWhile {
+    // mostrar por consola los nros de 1 al 100
+  /**  public static void main(String[] arg){
+        int x;
+        x=-50;
+        while (x <= 10) {
+            System.out.print(x);
+            System.out.print(" / ");
+            x = x + 1;
+        }
+    }*/
+  //While: por teclado se ingresen 10 nros , luego muestre la suma y el promedio
+
 }
